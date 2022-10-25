cc911869fe54c96644173f1dadeedf24bbd0e9ed
Congrak <rodricelis@gmail.com>
2022-10-25 22:59:41 +0000

    Random Gen by Rodrigo Celis

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const type = ["heart", "club", "spade", "dimond"];
+  const element = document.querySelector(".body");
+
+  let printNumber = number[Math.floor(Math.random() * number.length)];
+  let printType = type[Math.floor(Math.random() * type.length)];
+
+  if (printType == "heart" || printType == "dimond")
+    element.style.color = "red";
+  else element.style.color = "black";
+
+  element.innerHTML = printNumber;
+  document.querySelector(".card").className = "card" + " " + printType;
 };
